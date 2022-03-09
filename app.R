ba7a080a3eeffb265769ce46ed35e33ede8a4a42
christopheralex <christopher.a0@gmail.com>
2022-03-08 18:12:24 -0800

    feat:Adds graph and widgets

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 
 library(dash)
 # library(dashCoreComponents)
-# library(dashHtmlComponents)
+library(dashHtmlComponents)
 # library(dashBootstrapComponents)
 library(ggplot2)
 library(plotly)
@@ -11,33 +11,51 @@ app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
 data <- readr::read_csv(here::here('data', 'spotify.csv'))
 
+data <- tidyr::drop_na(data)
+
+artist <- unique(data$track_artist) %>% purrr::map(function(art) list(label = art, value = art))
+
 app$layout(
     dbcContainer(
         dbcRow(
             list(
+                htmlH1('Dashr spotify-ex'),
                 dbcCol(
                     list(
-                        htmlLabel('Left'),
+                        htmlLabel('Artist'),
                         dccDropdown(
-                            options = unique(data$track_artist) |> 
-                                purrr::map(function(value) list(label = value, value = value)),
-                            value = 'artist-id'
+                            id = 'artist-id',
+                            options = artist,
+                            # options = data %>% colnames %>% purrr::map(function(col) list(label = col, value = col)),
+                            value = 'artist'
                         )
                     )
                 ),
                 dbcCol(
                     list(
-                        htmlLabel('Right'),
-                        dccDropdown(
-                            options = list(list(label = "New York City", value = "NYC"),
-                                           list(label = "San Francisco", value = "SF")),
-                            value = 'SF'
-                        )
+                        htmlH4('Artist popularity record'),
+                        dccGraph(id='artist_pop_hist_id')
                     )
                 )
             )
         ), style = list('max-width' = '85%')  # Change left/right whitespace for the container
     )
+)
+
+app$callback(
+    output('artist_pop_hist_id', 'figure'),
+    list(input('artist-id', 'value')),
+    function(xcol="Ed Sheeran") {
+        chart <- ggplot2::ggplot(data |> dplyr::filter(track_artist == xcol)) + aes(
+            x = track_popularity ) + ggplot2::geom_histogram() + ggplot2::geom_vline(
+                data = data |> dplyr::filter(track_artist == xcol),
+                aes(xintercept = mean(track_popularity),
+                    colour="red")) + ggplot2::labs(
+                        x = "Track popularity",
+                        y = "Count",
+                        colour="Mean popluarity" ) 
+        ggplotly(chart) %>% layout(dragmode = 'select')
+    }
 )
 
 app$run_server(debug = T)
