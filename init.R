50c2df29fec954e64a18982e0985c18c06363a9f
christopheralex <christopher.a0@gmail.com>
2022-03-10 16:15:37 -0800

    patch:Fixes init.R

--- init.R
+++ init.R
@@ -9,5 +9,6 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes'))
+install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes', 'plotly',
+                   'dashHtmlComponents','ggplot'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
